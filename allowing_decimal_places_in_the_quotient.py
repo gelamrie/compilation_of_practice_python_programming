efd4c40055c021f105a190b759b4d44c43ffb7b8
Angela <angeladejuan55@gmail.com>
2025-03-08 21:00:35 +0800

    pseudocode created

---
+++ allowing_decimal_places_in_the_quotient.py
@@ -0,0 +1,5 @@
+#Ask user to enter two numbers that will be stored in num_1 and num_2
+#Create formula to divide the two numbers
+#Check if num_2 is not equal to zero
+#If true, solve for the quotient and display the result with decimal places
+#If num_2 is equal to zero, diplay "Division by zero is not allowed"
