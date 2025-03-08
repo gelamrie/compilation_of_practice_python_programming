a4ec3e415470942d8f9cde20320e056cbd0b1f14
Angela <angeladejuan55@gmail.com>
2025-03-08 19:35:48 +0800

    created pseudocode

---
+++ checking_if_the_two_numbers_are_equal.py
@@ -0,0 +1,4 @@
+#Ask the user to enter two numbers to be stored in num_1 and num_2
+#Checking if the two numbers are equal
+#Print "equal" if the numbers are equal
+#Print "not equal" if the numbers are not equal
