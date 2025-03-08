9ea3dcf39d588e0d3f3c2d758c2c98f4951ad4a2
Angela <angeladejuan55@gmail.com>
2025-03-08 20:34:03 +0800

    formula and print results

--- adding_two_numbers.py
+++ adding_two_numbers.py
@@ -3,5 +3,7 @@ num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
 
 #Create a formula to evaluate the numbers entered by the user
+sum = num_1 + num_2
 
 #Print the results
+print ("The sum of the two numbers is", sum, end = ".")
