8d74b3cfee7c34f8cf5f658770e8c83f68f66042
Angela <angeladejuan55@gmail.com>
2025-03-08 21:06:20 +0800

    checking and creating formula

--- allowing_decimal_places_in_the_quotient.py
+++ allowing_decimal_places_in_the_quotient.py
@@ -2,6 +2,6 @@
 num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
 
-#Check if num_2 is not equal to zero
-#If true, solve for the quotient and display the result with decimal places
+if num_2 != 0: #Check if num_2 is not equal to zero
+    quotient = num_1 / num_2 #If true, solve for the quotient 
 #If num_2 is equal to zero, diplay "Division by zero is not allowed"
