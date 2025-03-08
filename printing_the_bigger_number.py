65a428a1fb4a1a03d765e52be08bc77111fa7884
Angela <angeladejuan55@gmail.com>
2025-03-08 18:48:36 +0800

    creating the pseudocode

---
+++ printing_the_bigger_number.py
@@ -0,0 +1,5 @@
+#Ask the user to enter two numbers to be stored in num_1 and num_2
+#Compare the numbers to see which one is bigger 
+#Print the results
+    #print num_1 if it's bigger than num_2
+    #print num_2 if it's bigger than num_1
