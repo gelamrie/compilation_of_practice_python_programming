057bc10c8ccf80e11cd965a00276f167dd8e1625
Angela <angeladejuan55@gmail.com>
2025-03-08 18:55:20 +0800

    comparing the numbers and print results

--- printing_the_bigger_number.py
+++ printing_the_bigger_number.py
@@ -1,7 +1,8 @@
 #Ask the user to enter two numbers to be stored in num_1 and num_2
 num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
-#Compare the numbers to see which one is bigger 
-#Print the results
-    #print num_1 if it's bigger than num_2
-    #print num_2 if it's bigger than num_1
+
+if num_1 > num_2: #Compare the numbers to see which one is bigger 
+    print("The bigger number is", num_1) #print num_1 if it's bigger than num_2
+else: 
+    print("The bigger number is", num_2) #print num_2 if it's bigger than num_1
