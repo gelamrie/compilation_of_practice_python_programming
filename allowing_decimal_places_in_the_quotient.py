b0a3c92f5ff5f85f0224faf0515ec36cdf2f91c4
Angela <angeladejuan55@gmail.com>
2025-03-08 21:10:27 +0800

    print results

--- allowing_decimal_places_in_the_quotient.py
+++ allowing_decimal_places_in_the_quotient.py
@@ -4,4 +4,8 @@ num_2 = int(input("Enter the second number: "))
 
 if num_2 != 0: #Check if num_2 is not equal to zero
     quotient = num_1 / num_2 #If true, solve for the quotient 
+    print (f"The quotient of the two numbers is {quotient:2f}.") #Display the quotient with decimal places
+
 #If num_2 is equal to zero, diplay "Division by zero is not allowed"
+else:
+    print("Division by zero is not allowed")
