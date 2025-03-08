6fe871f41747ec8f2cca21061122c782716dbc78
Angela <angeladejuan55@gmail.com>
2025-03-08 21:04:45 +0800

    ask user to input two numbers

--- allowing_decimal_places_in_the_quotient.py
+++ allowing_decimal_places_in_the_quotient.py
@@ -1,5 +1,7 @@
 #Ask user to enter two numbers that will be stored in num_1 and num_2
-#Create formula to divide the two numbers
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 #Check if num_2 is not equal to zero
 #If true, solve for the quotient and display the result with decimal places
 #If num_2 is equal to zero, diplay "Division by zero is not allowed"
