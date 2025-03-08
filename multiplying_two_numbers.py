0affdd7deff4a21a87b795f6943c6d1acba0d815
Angela <angeladejuan55@gmail.com>
2025-03-08 20:42:16 +0800

    formula for multiplication and printing results

--- multiplying_two_numbers.py
+++ multiplying_two_numbers.py
@@ -1,3 +1,9 @@
 #Ask user to input two numbers
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 #Create the formula thal will multiply the two numbers
+product = num_1 * num_2
+
 #Print results
+print("The product of the two numbers is", product, end = ".")
