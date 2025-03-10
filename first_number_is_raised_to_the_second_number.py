41628bc3d95630393ee01add478fe0b02aea7805
Angela <angeladejuan55@gmail.com>
2025-03-10 22:33:17 +0800

    created the formula

--- first_number_is_raised_to_the_second_number.py
+++ first_number_is_raised_to_the_second_number.py
@@ -3,4 +3,6 @@ num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
 
 #Create the formula and compute the result by raising the base to the exponent
+result = num_1**num_2
+
 #Print result 
