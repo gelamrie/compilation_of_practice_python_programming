ffa15b7034e0f6e464583e027fc0a8bd39eef777
Angela <angeladejuan55@gmail.com>
2025-03-10 22:55:31 +0800

    results

--- sum_of_10_numbers.py
+++ sum_of_10_numbers.py
@@ -4,4 +4,5 @@ total = 0
 #Loop 10 times to get user input
 for i in range(10):
     total += float(input(f"Enter a number: ")) #Add each input number to the sum
-#Print the total sum or the result
+    
+print(f"The sum of all numbers is {total}") #Print the total sum or the result
