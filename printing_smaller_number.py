dc64a2db74657ebe7e39d27012d1986665420ee8
Angela <angeladejuan55@gmail.com>
2025-03-12 20:17:59 +0800

    pseudocode created

---
+++ printing_smaller_number.py
@@ -0,0 +1,4 @@
+#Ask the user to enter two numbers to be stored in num_1 and num_2
+#Compare the numbers to see which one is smaller
+#Print num_1 if it's smaller than num_2
+#Print num_2 if it's smaller than num_1
