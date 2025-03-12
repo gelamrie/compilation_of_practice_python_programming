5d15779ab8fce402b1863ce0c26741b305b6ecea
Angela <angeladejuan55@gmail.com>
2025-03-12 09:52:27 +0800

    checking if even and printing results

--- printing_even_numbers.py
+++ printing_even_numbers.py
@@ -1,4 +1,4 @@
 #Use a for loop to iterate from 0 to 100
 for i in range(0, 101, 2):
-#Check if the number is even
-#Print the even numbers
+    if i % 2 == 0: #Check if the number is even
+        print(i) #Print the even numbers
