bbcd3e280c380166d22c9ed8d33daddfb55d48ef
Angela <angeladejuan55@gmail.com>
2025-03-12 20:57:28 +0800

    pseudocode created

--- quotient_of_the_two_numbers_without_the_decimal_point.py
+++ quotient_of_the_two_numbers_without_the_decimal_point.py
@@ -1,1 +1,5 @@
-
+#Ask user to enter two numbers that will be stored in num_1 and num_2
+#Check if num_2 is not equal to zero
+#If true, solve for the quotient 
+#Display the quotient without decimal places
+#If num_2 is equal to zero, diplay "Division by zero is not allowed."
