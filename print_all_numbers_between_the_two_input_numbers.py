de8e39de21c2fcb06288298f51f1b9aca5aec84b
Angela <angeladejuan55@gmail.com>
2025-03-12 23:54:43 +0800

    user input

--- print_all_numbers_between_the_two_input_numbers.py
+++ print_all_numbers_between_the_two_input_numbers.py
@@ -1,3 +1,6 @@
 #Get input from the user
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 #Ensure 'num_1' is the smaller number
 #Print numbers between 'num_1' and 'num_2'
