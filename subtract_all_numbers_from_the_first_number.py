ee1d21690342698ae575c3066489963bf5f7e6ae
Angela <angeladejuan55@gmail.com>
2025-03-12 23:17:41 +0800

    input 10 numbers then add them to the number list

--- subtract_all_numbers_from_the_first_number.py
+++ subtract_all_numbers_from_the_first_number.py
@@ -1,5 +1,10 @@
 #Initialize an empty list to store numbers
 numbers = []
+
 #Ask the user to input 10 numbers
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+    
 #Compute the result by subtracting the sum of remaining numbers
 #Print the result
