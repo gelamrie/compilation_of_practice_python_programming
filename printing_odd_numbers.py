0d356fde02e570bfe663394560ed660322c82626
Angela <angeladejuan55@gmail.com>
2025-03-12 23:39:57 +0800

    while loop

--- printing_odd_numbers.py
+++ printing_odd_numbers.py
@@ -1,5 +1,5 @@
 #Initialize a counter at 1 
 i = 1
-#While the counter is less than or equal to 100:
+while i <= 100: #While the counter is less than or equal to 100:
     #Print the counter if it is odd
     #Increment the counter by 2
