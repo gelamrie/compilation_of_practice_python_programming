0ca37ac3e1f8e661e8bd990cc1e691590ed2b41e
Angela <angeladejuan55@gmail.com>
2025-03-12 20:51:08 +0800

    input two numbers

--- difference_of_the_two_numbers.py
+++ difference_of_the_two_numbers.py
@@ -1,3 +1,6 @@
 #Ask user to enter two numbers
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 #Create a formula to evaluate the numbers entered by the user
 #Print the results
