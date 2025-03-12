fd423c40a0e641b654d322e07376ae70c072cbcc
Angela <angeladejuan55@gmail.com>
2025-03-12 09:43:37 +0800

    increment

--- counting_odd_numbers.py
+++ counting_odd_numbers.py
@@ -2,5 +2,5 @@
 odd_num = 0
 for i in range(10): #Loop 10 times to get user input
     if int(input(f"Enter a number: ")) % 2 != 0: #Check if the number is odd
-#If odd, increment the counter
+        odd_num += 1 #If odd, increment the counter
 #Print the count of odd numbers
