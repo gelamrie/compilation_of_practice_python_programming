e37f6663a62eb75c89248c7bdeb46c946f0c6365
Angela <angeladejuan55@gmail.com>
2025-03-12 23:29:10 +0800

    loop

--- counting_even_numbers.py
+++ counting_even_numbers.py
@@ -1,7 +1,6 @@
 #Initialize a counter for even numbers to 0
 even_num = 0 
-
-#Loop 10 times to get user input
+for i in range(10): #Loop 10 times to get user input
 #Check if the number is even
 #If even, increment the counter
 #Print the count of even numbers
