86694a33186f21020dbc5aa1f46183b67e3cb0e4
Angela <angeladejuan55@gmail.com>
2025-03-12 20:44:47 +0800

    condition

--- checking_if_the_two_numbers_are_not_equal.py
+++ checking_if_the_two_numbers_are_not_equal.py
@@ -3,5 +3,6 @@ num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
 
 #Checking if the two numbers are not equal
+if num_1 != num_2:
 #Print "not equal" if the numbers are not equal
 #Print "equal" if the numbers are equal
