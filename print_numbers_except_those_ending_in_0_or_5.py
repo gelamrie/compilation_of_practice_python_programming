93455a9c784a293eb1c634bc2a2ee6bba45660b8
Angela <angeladejuan55@gmail.com>
2025-03-12 23:47:20 +0800

    condition and print results

--- print_numbers_except_those_ending_in_0_or_5.py
+++ print_numbers_except_those_ending_in_0_or_5.py
@@ -1,3 +1,4 @@
 #Loop from 0 to 100
 for i in range(101): 
-#Print the number only if it does not end in 0 or 5
+    if i % 10 != 0 and i % 10 != 5:
+        print(i) #Print the number only if it does not end in 0 or 5
