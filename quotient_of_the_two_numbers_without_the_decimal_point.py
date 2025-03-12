f6ecddb4abc8156d9ba905cdf111c6f20302cb5a
Angela <angeladejuan55@gmail.com>
2025-03-12 20:58:55 +0800

    pseudocode created

--- quotient_of_the_two_numbers_without_the_decimal_point.py
+++ quotient_of_the_two_numbers_without_the_decimal_point.py
@@ -1,4 +1,7 @@
 #Ask user to enter two numbers that will be stored in num_1 and num_2
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 #Check if num_2 is not equal to zero
 #If true, solve for the quotient 
 #Display the quotient without decimal places
