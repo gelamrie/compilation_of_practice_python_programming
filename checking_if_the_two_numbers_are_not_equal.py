2536ffe4ba74508b5cd4d11631994aebd00939b9
Angela <angeladejuan55@gmail.com>
2025-03-12 20:39:41 +0800

    pseudocode created

--- checking_if_the_two_numbers_are_not_equal.py
+++ checking_if_the_two_numbers_are_not_equal.py
@@ -1,4 +1,7 @@
 #Ask the user to enter two numbers to be stored in num_1 and num_2
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 #Checking if the two numbers are not equal
 #Print "not equal" if the numbers are not equal
 #Print "equal" if the numbers are equal
