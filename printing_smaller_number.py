6a02b53d27744a6b6cfd2b1329533a0a40b6fac4
Angela <angeladejuan55@gmail.com>
2025-03-12 20:20:32 +0800

    if-else statement

--- printing_smaller_number.py
+++ printing_smaller_number.py
@@ -1,6 +1,7 @@
 #Ask the user to enter two numbers to be stored in num_1 and num_2
 num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
-#Compare the numbers to see which one is smaller
+
+if num_1 > num_2: #Compare the numbers to see which one is smaller
 #Print num_1 if it's smaller than num_2
 #Print num_2 if it's smaller than num_1
