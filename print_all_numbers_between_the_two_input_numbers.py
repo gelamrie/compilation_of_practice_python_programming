7a1974fe8128d70e0d34e447d13a994a9482bba8
Angela <angeladejuan55@gmail.com>
2025-03-12 23:56:21 +0800

    ensure 'num_1' is the smaller number

--- print_all_numbers_between_the_two_input_numbers.py
+++ print_all_numbers_between_the_two_input_numbers.py
@@ -3,4 +3,7 @@ num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
 
 #Ensure 'num_1' is the smaller number
+if num_1 > num_2:
+    num_1, num_2 = num_2, num_2 
+    
 #Print numbers between 'num_1' and 'num_2'
