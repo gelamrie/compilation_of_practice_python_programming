03222bc5c4196adf24f8bbaaad6010429b6910fc
Angela <angeladejuan55@gmail.com>
2025-03-12 10:00:36 +0800

    checking numbers and printing results

--- printing_numbers_except_those_ending_in_zero.py
+++ printing_numbers_except_those_ending_in_zero.py
@@ -1,4 +1,4 @@
 #Use a for loop to iterate from 0 to 100
 for i in range(101):
-#Check if the number does not end in zero 
-#Print the numbers that meet the condition
+    if i % 10 != 0: #Check if the number does not end in zero 
+        print(i) #Print the numbers that meet the condition
