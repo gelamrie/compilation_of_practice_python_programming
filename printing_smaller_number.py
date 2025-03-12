1fd32840028a30a381b34bd9b8527055fffc755a
Angela <angeladejuan55@gmail.com>
2025-03-12 20:27:16 +0800

    printing results

--- printing_smaller_number.py
+++ printing_smaller_number.py
@@ -2,6 +2,7 @@
 num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
 
-if num_1 > num_2: #Compare the numbers to see which one is smaller
-#Print num_1 if it's smaller than num_2
-#Print num_2 if it's smaller than num_1
+if num_1 < num_2: #Compare the numbers to see which one is smaller
+    print("The smaller number is", num_1, end= ".") #Print num_1 if it's smaller than num_2
+else: 
+    print("The smaller number is", num_2, end= ".") #Print num_2 if it's smaller than num_1
