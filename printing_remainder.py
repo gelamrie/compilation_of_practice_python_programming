bbeb6dbfb94947714daf0a64b9419141e25da978
Angela <angeladejuan55@gmail.com>
2025-03-12 23:04:40 +0800

    formula and print results

--- printing_remainder.py
+++ printing_remainder.py
@@ -3,4 +3,5 @@ num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
 
 #Compute the remainder when the first number is divided by the second
-#Print the remainder
+remainder = num_1 % num_2
+print("The remainder is", remainder, end= ".") #Print the remainder
