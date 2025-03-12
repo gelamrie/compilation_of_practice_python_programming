4adcec0c07bd3f03369daea5057909837787dad6
Angela <angeladejuan55@gmail.com>
2025-03-12 23:31:41 +0800

    checking if even and adding count if it is

--- counting_even_numbers.py
+++ counting_even_numbers.py
@@ -1,6 +1,7 @@
 #Initialize a counter for even numbers to 0
 even_num = 0 
 for i in range(10): #Loop 10 times to get user input
-#Check if the number is even
-#If even, increment the counter
+    if int(input("Enter a number: ")) % 2 == 0: #Check if the number is even
+        evennum += 1 #If even, increment the counter
+
 #Print the count of even numbers
