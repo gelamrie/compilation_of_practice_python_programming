1ab4d2f7e08145f075a730dc3dd64ae2a0b30349
Angela <angeladejuan55@gmail.com>
2025-03-12 20:19:07 +0800

    input numbers

--- printing_smaller_number.py
+++ printing_smaller_number.py
@@ -1,4 +1,6 @@
 #Ask the user to enter two numbers to be stored in num_1 and num_2
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
 #Compare the numbers to see which one is smaller
 #Print num_1 if it's smaller than num_2
 #Print num_2 if it's smaller than num_1
