23946d8beabfa1221e8530fc0b61d09f06471442
Angela <angeladejuan55@gmail.com>
2025-03-13 15:16:44 +0800

    identify numbers that appear more than once

--- printing_numbers_that_have_duplicate.py
+++ printing_numbers_that_have_duplicate.py
@@ -1,5 +1,7 @@
 #Ask user to input 10 numbers and store them in a list
 numbers = [int(input("Enter a number: ")) for i in range (10)]
 
-#Identify numbers that appear mutiple time in the list 
+#Identify numbers that appear mutiple time in the list
+duplicates = sorted(set(num for num in numbers if numbers.count(num) > 1))
+
 #Print the numbers that has duplicate
