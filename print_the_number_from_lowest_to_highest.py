05567f5e757a3025c0eaf71db5a37bb825a0742c
Angela <angeladejuan55@gmail.com>
2025-03-13 14:46:56 +0800

    while loop and input numbers in the list

--- print_the_number_from_lowest_to_highest.py
+++ print_the_number_from_lowest_to_highest.py
@@ -1,6 +1,7 @@
 #Create an empty list
 numbers = []
-#While loop
-    #Add the user input to the list
-    #Break or stop if invalid input entered
+while True: #While loop
+    try: 
+        numbers.append(int(input("Enter a number: ")))#Add the user input to the list
+   #Break or stop if invalid input entered
 #Print the number from lowest to highest
