567d248e539eba25edbc83257d70fb211628fa90
Angela <angeladejuan55@gmail.com>
2025-03-13 15:57:52 +0800

    identify no. with most duplicates and print results

--- print_number_with_the_most_number_of_duplicate.py
+++ print_number_with_the_most_number_of_duplicate.py
@@ -2,9 +2,11 @@
 numbers = []
 while True: #While loop
     try: 
-        numbers.append(int(input("Enter a number: ")))#Add user input to the list
+        numbers.append(int(input("Enter a number: "))) #Add user input to the list
     except ValueError:
         break #Break or stop if an invalid input was entered
-#Identify the number that has the most duplicate
-    #Print the number with most duplicates
-    #If there's no number entered, print "No numbers entered."
+if numbers: 
+    most_common = max(set(numbers), key=numbers.count) #Identify the number that has the most duplicate
+    print("The number with most duplicates is", most_common, end= ".") #Print the number with most duplicates
+else: 
+    print("No numbers entered.") #If there's no number entered, print "No numbers entered.
