ea3a9c40a72ff88326fcb8e2ed6bb23b818de101
Angela <angeladejuan55@gmail.com>
2025-03-13 16:06:40 +0800

    break if invalid input

--- printing_the_highest_number.py
+++ printing_the_highest_number.py
@@ -3,7 +3,8 @@ numbers = []
 while True: #while loop
     try: 
         numbers.append(int(input("Enter a number: "))) #Add user input to the list
-    #Break if invalid input was entered
+    except ValueError: 
+        break #Break if invalid input was entered
 #Identify the highest number
     #Print the highest number
 #Else, print "No numbers entered." if there are no numbers detected
