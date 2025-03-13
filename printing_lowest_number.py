20e0f2defe1e8b3e8b6e6f2b268b38e3b50afc81
Angela <angeladejuan55@gmail.com>
2025-03-13 14:25:49 +0800

    pseudocode created

---
+++ printing_lowest_number.py
@@ -0,0 +1,7 @@
+#Create an empty list
+#While loop
+    #Add the user input to the list
+    #Stop when an invalid input is entered
+#Display result
+    #Print the lowest number if there is any
+    #Print "No numbers entered" if no numbers were entered
