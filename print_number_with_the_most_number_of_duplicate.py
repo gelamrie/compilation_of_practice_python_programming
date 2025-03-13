ad90a30a99f1ff3f9c009bf2564d167e407db2ee
Angela <angeladejuan55@gmail.com>
2025-03-13 15:48:20 +0800

    pseudocode created

---
+++ print_number_with_the_most_number_of_duplicate.py
@@ -0,0 +1,7 @@
+#Create an empty list
+#While loop
+    #Add user input to the list
+    #Break or stop if an invalid input was entered
+#Identify the number that has the most duplicate
+    #Print the number with most duplicates
+    #If there's no number entered, print "No numbers entered."
