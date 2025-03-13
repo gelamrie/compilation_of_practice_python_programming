70c8040d8959846ec0361623d2a1d11273d7e852
Angela <angeladejuan55@gmail.com>
2025-03-13 14:29:22 +0800

    break if invalid input entered

--- printing_lowest_number.py
+++ printing_lowest_number.py
@@ -3,7 +3,9 @@ numbers = []
 while True: #While loop
     try: 
         numbers.append(int(input("Enter a number: "))) #Add the user input to the list
-    #Stop when an invalid input is entered
+    except ValueError:
+        break #Stop when an invalid input is entered
+
 #Display result
     #Print the lowest number if there is any
     #Print "No numbers entered" if no numbers were entered
