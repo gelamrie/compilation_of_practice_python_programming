586d3a6a78df918ff53d5b10dc7596e4db4fd736
Angela <angeladejuan55@gmail.com>
2025-03-13 15:52:29 +0800

    break if invalid input was entered

--- print_number_with_the_most_number_of_duplicate.py
+++ print_number_with_the_most_number_of_duplicate.py
@@ -3,7 +3,8 @@ numbers = []
 while True: #While loop
     try: 
         numbers.append(int(input("Enter a number: ")))#Add user input to the list
-    #Break or stop if an invalid input was entered
+    except ValueError:
+        break #Break or stop if an invalid input was entered
 #Identify the number that has the most duplicate
     #Print the number with most duplicates
     #If there's no number entered, print "No numbers entered."
