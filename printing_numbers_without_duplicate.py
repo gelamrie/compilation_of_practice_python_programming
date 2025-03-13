6d44c4d75dbba59c0ed8ddb04c11c5c4c290e3de
Angela <angeladejuan55@gmail.com>
2025-03-13 11:52:25 +0800

    input numbers and store in a list

--- printing_numbers_without_duplicate.py
+++ printing_numbers_without_duplicate.py
@@ -1,3 +1,5 @@
 #Ask user to input 10 numbers and store them in a list
+numbers = [int(input("Enter a number: ")) for i in range (10)]
+
 #Identify numbers that appear only once in the list 
 #Print the unique numbers
