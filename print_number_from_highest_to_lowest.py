286d82b69ee390fc2904e12188b22b2932391521
Angela <angeladejuan55@gmail.com>
2025-03-13 16:28:00 +0800

    pseudocode created

---
+++ print_number_from_highest_to_lowest.py
@@ -0,0 +1,9 @@
+#Create an empty list
+#while loop
+    #Add the numbers from user input to the list
+    #Break if invalid input was entered
+
+#Identify the numbers from highest to lowest
+    #Use revserse=True to modify the arrangement in descending order 
+    #Print numbers from highest to lowest
+    #Print "No numbers entered." if there's no number detected
