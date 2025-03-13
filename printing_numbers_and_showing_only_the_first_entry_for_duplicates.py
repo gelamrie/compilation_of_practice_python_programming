ef77b7e3046c7fccc04717ba0321818ddddcc1e4
Angela <angeladejuan55@gmail.com>
2025-03-13 13:39:33 +0800

    print the list

--- printing_numbers_and_showing_only_the_first_entry_for_duplicates.py
+++ printing_numbers_and_showing_only_the_first_entry_for_duplicates.py
@@ -4,5 +4,6 @@ for i in range (10): #Create a loop to get the user input
     num = int(input("Enter a number: "))
     if num not in numbers: #Add number only if it is not in the list already
         numbers.append(num)
-        
+
 #Print the list 
+print("Here are the numbers with first occurrences only:", numbers, end= ".")
