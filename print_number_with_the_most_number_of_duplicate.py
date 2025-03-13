68e8044c0e37cea599e1b79846825f199a062331
Angela <angeladejuan55@gmail.com>
2025-03-13 15:51:39 +0800

    while loop and ask user to input numbers

--- print_number_with_the_most_number_of_duplicate.py
+++ print_number_with_the_most_number_of_duplicate.py
@@ -1,7 +1,8 @@
 #Create an empty list
 numbers = []
-#While loop
-    #Add user input to the list
+while True: #While loop
+    try: 
+        numbers.append(int(input("Enter a number: ")))#Add user input to the list
     #Break or stop if an invalid input was entered
 #Identify the number that has the most duplicate
     #Print the number with most duplicates
