c7e67dcd1212a60f82cbdb9860dbceebb2e20b9b
Angela <angeladejuan55@gmail.com>
2025-03-13 16:02:43 +0800

    pseudocode created

---
+++ printing_the_highest_number.py
@@ -0,0 +1,7 @@
+#Create an empty list
+#while loop
+    #Add user input to the list
+    #Break if invalid input was entered
+#Identify the highest number
+    #Print the highest number
+#Else, print "No numbers entered." if there are no numbers detected
