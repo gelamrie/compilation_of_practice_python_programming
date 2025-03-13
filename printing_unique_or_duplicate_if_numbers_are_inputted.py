d16fba3ff7c6f7c9ecaeb014e69e76ce9b05871f
Angela <angeladejuan55@gmail.com>
2025-03-13 14:12:59 +0800

    fixing the pseudocode and try to print results

--- printing_unique_or_duplicate_if_numbers_are_inputted.py
+++ printing_unique_or_duplicate_if_numbers_are_inputted.py
@@ -1,12 +1,13 @@
 #Create an empty list 
 numbers = []
-while True: #Create while loop 
+while True: #Create a while loop 
     try:
-        num = int(input("Enter a number: "))#Get user input
+        num = int(input("Enter a number: ")) #Get user input
     
-    if num in numbers: #Check if the number is already in the list 
-        print("Duplicate") #Print "duplicate" if number is in the list
-    else: 
-        print("Unique") #Print "unique" if number is not in the list
-
-#Stop when an invalid input is entered 
+        if num in numbers: #Check if the number is already in the list 
+            print("Duplicate") #Print "duplicate" if number is in the list
+        else: 
+            print("Unique") #Print "unique" if number is not in the list
+            numbers.append(num) #Add unique numbers to the list
+    except ValueError: #Stop when an invalid input is entered 
+        break 
