58d8447782af2aeb6bb13bd5b09f32900d1d6709
Angela <angeladejuan55@gmail.com>
2025-03-13 16:30:48 +0800

    break if input is invalid

--- print_number_from_highest_to_lowest.py
+++ print_number_from_highest_to_lowest.py
@@ -3,7 +3,8 @@ numbers = []
 while True: #while loop
     try: 
         numbers.append(int(input("Enter a number: ")))#Add the numbers from user input to the list
-    #Break if invalid input was entered
+    except ValueError:
+        break #Break if invalid input was entered
 
 #Identify the numbers from highest to lowest
     #Use revserse=True to modify the arrangement in descending order 
