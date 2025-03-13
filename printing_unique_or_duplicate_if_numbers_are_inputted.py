ff0e16d9b8fc54f89f6b49e180d77664a1f6e83f
Angela <angeladejuan55@gmail.com>
2025-03-13 14:05:44 +0800

    checking and print duplicate or unique

--- printing_unique_or_duplicate_if_numbers_are_inputted.py
+++ printing_unique_or_duplicate_if_numbers_are_inputted.py
@@ -3,7 +3,10 @@ numbers = []
 while True: #Create while loop 
     try:
         num = int(input("Enter a number: "))#Get user input
-    #Check if the number is already in the list 
-    #Print "unique" if number is not in the list
-    #Print "duplicate" if number is in the list
+    
+    if num in numbers: #Check if the number is already in the list 
+        print("Duplicate") #Print "duplicate" if number is in the list
+    else: 
+        print("Unique") #Print "unique" if number is not in the list
+
 #Stop when an invalid input is entered 
