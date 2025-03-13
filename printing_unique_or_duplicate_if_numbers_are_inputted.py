4bbb092b7edd54a9cc8839c21c57cadd1bb8cb4e
Angela <angeladejuan55@gmail.com>
2025-03-13 13:58:29 +0800

    pseudocode created

---
+++ printing_unique_or_duplicate_if_numbers_are_inputted.py
@@ -0,0 +1,7 @@
+#Create an empty list 
+#Create while loop 
+    #Get user input
+    #Check if the number is already in the list 
+    #Print "unique" if number is not in the list
+    #Print "duplicate" if number is in the list
+#Stop when an invalid input is entered 
