2f5c8f941b17e353319f2823c7fc9177aca7133f
Angela <angeladejuan55@gmail.com>
2025-03-13 16:44:22 +0800

    identify and creating formula for average

--- printing_the_average.py
+++ printing_the_average.py
@@ -5,7 +5,9 @@ while True: #while loop
         numbers.append(int(input("Enter a number: "))) #Add the numbers from the user input to the list
     except ValueError: 
         break #Break if invalid input was entered
+
 #Compute for the average
-    #Create formula
+if numbers: 
+    average = sum(numbers) / len (numbers) #Create formula
     #Print the average
     #Print "No numbers entered" if there's no number detected
