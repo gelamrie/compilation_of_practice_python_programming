f9e7bdd6f7dc6d2a00d5d066ff8df932e50710f5
Angela <angeladejuan55@gmail.com>
2025-03-13 16:39:50 +0800

    pseudocode created

---
+++ printing_the_average.py
@@ -0,0 +1,8 @@
+#Initialized an empty list
+#while loop
+    #Add the numbers from the user input to the list
+    #Break if invalid input was entered
+#Compute for the average
+    #Create formula
+    #Print the average
+    #Print "No numbers entered" if there's no number detected
