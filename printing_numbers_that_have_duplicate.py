e6450e4a8bcd3fb84ed0cef79fe2ddd784e725a1
Angela <angeladejuan55@gmail.com>
2025-03-13 15:09:54 +0800

    input numbers

--- printing_numbers_that_have_duplicate.py
+++ printing_numbers_that_have_duplicate.py
@@ -1,3 +1,5 @@
 #Ask user to input 10 numbers and store them in a list
+numbers = [int(input("Enter a number: ")) for i in range (10)]
+
 #Identify numbers that appear mutiple time in the list 
 #Print the numbers that has duplicate
